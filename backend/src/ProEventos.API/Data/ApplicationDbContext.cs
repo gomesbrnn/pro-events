a43b749f3c3b95126734998e49e273cee1b593cb
gomesbrnn <brngomesds@gmail.com>
2023-02-06 15:46:16 -0300

    Mapeamento da entidade de Evento com o Entity Framework

--- backend/src/ProEventos.API/Data/ApplicationDbContext.cs
+++ backend/src/ProEventos.API/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Data
 {
@@ -6,5 +7,7 @@ namespace ProEventos.API.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         { }
+
+        public DbSet<Evento> Eventos { get; set; }
     }
 }
