2577e080663d7644bb49fbc5971a19a5e1b9ec3a
gomesbrnn <brngomesds@gmail.com>
2023-02-06 16:37:48 -0300

    Mapeamento da nova propriedade da entidade Evento pro banco de Dados

---
+++ backend/src/ProEventos.API/Data/Migrations/20230206193623_UpdateEvento.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProEventos.API.Data.Migrations
+{
+    public partial class UpdateEvento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Eventos",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Eventos");
+        }
+    }
+}
