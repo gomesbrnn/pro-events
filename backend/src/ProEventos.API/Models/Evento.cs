aed48d1fe8a71e628a43e860205d29b2964f5dd4
gomesbrnn <brngomesds@gmail.com>
2023-02-06 15:46:09 -0300

    Criação da entidade de Evento

---
+++ backend/src/ProEventos.API/Models/Evento.cs
@@ -0,0 +1,13 @@
+namespace ProEventos.API.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public string Data { get; set; }
+        public string Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
