84ff2d1c0dd764386d4d934bedf36f91e2aa561f
gomesbrnn <brngomesds@gmail.com>
2023-02-07 15:31:54 -0300

    criação dos componentes de Eventos e Palestrantes

---
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  public getEventos(): any {
+    return ""
+  }
+
+  ngOnInit(): void {
+    this.getEventos()
+  }
+
+}
