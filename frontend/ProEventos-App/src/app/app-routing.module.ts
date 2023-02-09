36d68a2722db48804bbb3e7b820a26073732bf28
gomesbrnn <brngomesds@gmail.com>
2023-02-08 21:05:41 -0300

    Criação das primeiras rotas da aplicação

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventosComponent } from './eventos/eventos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/eventos', pathMatch: 'full', data: { title: 'Home' } },
+  { path: 'eventos', component: EventosComponent, data: { title: 'Eventos' } }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
