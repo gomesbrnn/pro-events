e8b70fcace10d91b87462c457ca00fc826faf402
gomesbrnn <brngomesds@gmail.com>
2023-02-17 15:17:32 -0300

    Alteração de métodos do GeraoRepository

--- backend/src/ProEventos.Persistence/GeralRepository.cs
+++ backend/src/ProEventos.Persistence/GeralRepository.cs
@@ -23,11 +23,6 @@ namespace ProEventos.Persistence
         {
             _context.Update(entity);
         }
-        public void Delete<T>(T entity) where T : class
-        {
-            _context.Remove(entity);
-        }
-
         public void DeleteRange<T>(T[] entityArray) where T : class
         {
             _context.RemoveRange(entityArray);
--- backend/src/ProEventos.Persistence/Interfaces/IGeralRepository.cs
+++ backend/src/ProEventos.Persistence/Interfaces/IGeralRepository.cs
@@ -6,7 +6,6 @@ namespace ProEventos.Persistence.Interfaces
     {
         void Add<T>(T entity) where T : class;
         void Update<T>(T entity) where T : class;
-        void Delete<T>(T entity) where T : class;
         void DeleteRange<T>(T[] entityArray) where T : class;
         Task<bool> SaveChangesAsync();
     }
