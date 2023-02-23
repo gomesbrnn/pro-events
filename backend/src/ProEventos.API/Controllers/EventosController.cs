123ace0aa53a06d24558e58c5d707c36aebe2c67
gomesbrnn <brngomesds@gmail.com>
2023-02-22 23:04:39 -0300

    Implementação dos GETS de evento na controller de Eventos

--- backend/src/ProEventos.API/Controllers/EventosController.cs
+++ backend/src/ProEventos.API/Controllers/EventosController.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProEventos.Application.Interfaces;
 using ProEventos.Domain.Models;
 using ProEventos.Persistence.Context;
 
@@ -11,24 +14,79 @@ namespace ProEventos.API.Controllers
     [Route("api/v1/eventos")]
     public class EventosController : ControllerBase
     {
-        private ProEventosContext _context;
-        public EventosController(ProEventosContext context)
+        private readonly IEventoService _eventoService;
+        public EventosController(IEventoService eventoService)
         {
-            _context = context;
+            _eventoService = eventoService;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> GetEventos()
+        public async Task<IActionResult> GetEventos()
         {
-            var eventos = _context.Eventos.Where(evento => evento.Status == true).AsNoTracking().ToList();
-            return StatusCode(200, eventos);
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosAsync(true);
+
+                if (eventos is null) return NoContent();
+
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    titulo = "Não foi possivel tratar sua solicitação",
+                    status = 500,
+                    detalhes = ex.Message,
+                    timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssffff")
+                });
+            }
         }
 
         [HttpGet("{id}")]
-        public ActionResult GetEventoId(int id)
+        public async Task<IActionResult> GetEventoById(int id)
         {
-            var evento = _context.Eventos.FirstOrDefault(evento => evento.Id == id);
-            return StatusCode(200, evento);
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(id, true);
+
+                if (evento is null) return NoContent();
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    titulo = "Não foi possivel tratar sua solicitação",
+                    status = 500,
+                    detalhes = ex.Message,
+                    timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssffff")
+                });
+            }
+        }
+
+        [HttpGet("{tema}")]
+        public async Task<IActionResult> GetEventoByTema(string tema)
+        {
+            try
+            {
+                var evento = await _eventoService.GetAllEventosByTemaAsync(tema, true);
+
+                if (evento is null) return NoContent();
+
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    titulo = "Não foi possivel tratar sua solicitação",
+                    status = 500,
+                    detalhes = ex.Message,
+                    timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssffff")
+                });
+            }
         }
     }
 }
