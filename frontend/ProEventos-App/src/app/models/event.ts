b7b49a09c4e103b1194d5194ae9cbaac72992371
gomesbrnn <brngomesds@gmail.com>
2023-03-13 14:00:52 -0300

    feat: include new event relationships and props

--- frontend/ProEventos-App/src/app/models/event.ts
+++ frontend/ProEventos-App/src/app/models/event.ts
@@ -1,10 +1,18 @@
+import { Lot } from "./lot";
+import { SocialMedia } from "./social-media";
+import { Speaker } from "./speaker";
+
 export interface Event {
   id: number;
   city: string;
   date?: string;
   theme: string;
   amountPeople: number;
-  lot: string;
   imageURL: string;
+  phone: string;
+  email: string;
+  lots: Lot[];
+  socialMedia: SocialMedia[];
+  speakersEvents: Speaker[];
   status: boolean;
 }
