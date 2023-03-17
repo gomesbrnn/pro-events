1c222483cf03e246a6001fdba3c39346da87f171
gomesbrnn <brngomesds@gmail.com>
2023-03-17 14:52:42 -0300

    test: Recreating frontend test files

---
+++ frontend/ProEventos-App/src/app/models/social-media.spec.ts
@@ -0,0 +1,7 @@
+import { SocialMedia } from './social-media';
+
+describe('SocialMedia', () => {
+    it('should create an instance', () => {
+        expect(new SocialMedia()).toBeTruthy();
+    });
+});
