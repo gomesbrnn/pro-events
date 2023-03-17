312a7b634e8ad00d3aaddb6bba30991585b519b3
gomesbrnn <brngomesds@gmail.com>
2023-03-17 10:59:01 -0300

    refactor: Organizing app module imports

--- frontend/ProEventos-App/src/app/app.module.ts
+++ frontend/ProEventos-App/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { EventsComponent } from './@events/events.component';
 import { SpeakersComponent } from './speakers/speakers.component';
-import { NavComponent } from './nav/nav.component';
 
 import { EventService } from './services/event.service';
 import { DateTimeFormatPipe } from './helpers/date-time-format.pipe';
