eab932b22b13c1eaaf5d9e98f47e0bc9ada2a0e1
gomesbrnn <brngomesds@gmail.com>
2023-03-17 14:05:28 -0300

    feat: Add new routes

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsComponent } from './@events/events.component';
+import { SpeakersComponent } from './speakers/speakers.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/events', pathMatch: 'full', data: { title: 'Home' } },
-  { path: 'events', component: EventsComponent, data: { title: 'Events' } }
+  { path: 'events', component: EventsComponent, data: { title: 'Events' } },
+  { path: 'speakers', component: SpeakersComponent, data: { title: 'Speakers' } }
 ];
 
 @NgModule({
