897404428b1ccb184fc2ec5f2be6d34a495c6bba
gomesbrnn <brngomesds@gmail.com>
2023-03-17 15:36:11 -0300

    feat: Add routes for new components

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EventsComponent } from './components/events/events.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { SpeakersComponent } from './components/speakers/speakers.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/events', pathMatch: 'full', data: { title: 'Home' } },
   { path: 'events', component: EventsComponent, data: { title: 'Events' } },
-  { path: 'speakers', component: SpeakersComponent, data: { title: 'Speakers' } }
+  { path: 'speakers', component: SpeakersComponent, data: { title: 'Speakers' } },
+  { path: 'contacts', component: ContactsComponent, data: { title: 'Contacts' } },
+  { path: 'profile', component: ProfileComponent, data: { title: 'Profile' } },
+  { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } }
 ];
 
 @NgModule({
