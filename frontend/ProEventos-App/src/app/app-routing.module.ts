58ef7329f14181451e66114f197c3df83cfd561a
gomesbrnn <brngomesds@gmail.com>
2023-03-20 15:53:32 -0300

    feat: Making the event root component have children routes

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventDetailComponent } from './components/events/event-detail/event-detail.component';
+import { EventListComponent } from './components/events/event-list/event-list.component';
 import { EventsComponent } from './components/events/events.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SpeakersComponent } from './components/speakers/speakers.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full', data: { title: 'Home' } },
-  { path: 'events', component: EventsComponent, data: { title: 'Events' } },
+  {
+    path: 'events', component: EventsComponent, data: { title: 'Events' },
+    children: [
+      { path: 'list', component: EventListComponent },
+      { path: 'detail', component: EventDetailComponent },
+      { path: 'detail/:id', component: EventDetailComponent }
+    ]
+  },
   { path: 'speakers', component: SpeakersComponent, data: { title: 'Speakers' } },
   { path: 'contacts', component: ContactsComponent, data: { title: 'Contacts' } },
   { path: 'profile', component: ProfileComponent, data: { title: 'Profile' } },
