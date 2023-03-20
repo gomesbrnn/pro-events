c15a888e3ab668dd48fb2166032304544ab900d1
gomesbrnn <brngomesds@gmail.com>
2023-03-20 11:55:00 -0300

    fix: Route declaration order

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { SpeakersComponent } from './components/speakers/speakers.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full', data: { title: 'Home' } },
-  { path: '**', redirectTo: 'dashboard', pathMatch: 'full', data: { title: 'Home' } },
   { path: 'events', component: EventsComponent, data: { title: 'Events' } },
   { path: 'speakers', component: SpeakersComponent, data: { title: 'Speakers' } },
   { path: 'contacts', component: ContactsComponent, data: { title: 'Contacts' } },
   { path: 'profile', component: ProfileComponent, data: { title: 'Profile' } },
-  { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } }
+  { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full', data: { title: 'Home' } }
 ];
 
 @NgModule({
