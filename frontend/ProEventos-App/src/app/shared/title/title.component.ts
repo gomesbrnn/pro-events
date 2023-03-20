f424017e1cc6c20438aaa7e8fb652a27e1645aab
gomesbrnn <brngomesds@gmail.com>
2023-03-20 16:05:16 -0300

    feat: Dynamic function to list contents on global title component

--- frontend/ProEventos-App/src/app/shared/title/title.component.ts
+++ frontend/ProEventos-App/src/app/shared/title/title.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-title',
@@ -10,6 +11,12 @@ export class TitleComponent implements OnInit {
   @Input() title = '';
   @Input() subtitle?= '';
   @Input() listButton = false;
+
+  constructor(private router: Router) { }
+
+  public list(): void {
+    this.router.navigate([`/${this.title.toLocaleLowerCase()}/list`])
+  }
 
   ngOnInit(): void {
     ''
