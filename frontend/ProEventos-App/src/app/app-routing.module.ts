9b72a8b32e6d143bb8809808663f6ddf708eb1f2
gomesbrnn <brngomesds@gmail.com>
2023-03-23 14:02:02 -0300

    refactor: Adjusting routes order on app routing module

--- frontend/ProEventos-App/src/app/app-routing.module.ts
+++ frontend/ProEventos-App/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { SpeakersComponent } from './components/speakers/speakers.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full', data: { title: 'Home' } },
+  { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } },
   { path: 'events', redirectTo: 'events/list' },
   {
     path: 'events', component: EventsComponent, data: { title: 'Events' },
@@ -22,7 +23,6 @@ const routes: Routes = [
   { path: 'speakers', component: SpeakersComponent, data: { title: 'Speakers' } },
   { path: 'contacts', component: ContactsComponent, data: { title: 'Contacts' } },
   { path: 'profile', component: ProfileComponent, data: { title: 'Profile' } },
-  { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } },
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full', data: { title: 'Home' } }
 ];
 
