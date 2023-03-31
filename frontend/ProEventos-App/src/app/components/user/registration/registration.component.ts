959af0c49941655f6c91f397b1546568adfbc817
gomesbrnn <brngomesds@gmail.com>
2023-03-31 13:56:55 -0300

    feat: Implementing reactive forms on registration form

--- frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+  constructor(private formbuilder: FormBuilder) { }
 
+  registrationForm = this.formbuilder.group({
+
+    firstName: ['', [
+      Validators.required
+    ]],
+    lastName: ['', [
+      Validators.required
+    ]],
+    email: ['', [
+      Validators.required,
+      Validators.email
+    ]],
+    username: ['', [
+      Validators.required,
+      Validators.minLength(4)
+    ]],
+    password: ['', [
+      Validators.required,
+      Validators.min(8)
+    ]],
+    confirmPassword: ['', [
+      Validators.required,
+      Validators.min(8)
+    ]],
+
+  })
 }
