813decfd339df1d7d2e853306569c4e76f82a34e
gomesbrnn <brngomesds@gmail.com>
2023-04-17 15:47:02 -0300

    feat: reactive form configuration for profile form

--- frontend/ProEventos-App/src/app/components/user/profile/profile.component.ts
+++ frontend/ProEventos-App/src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, Validators } from '@angular/forms';
+import { FieldValidator } from 'src/app/helpers/field-validator';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  constructor(private formBuilder: FormBuilder) { }
 
+  formOptions: AbstractControlOptions = {
+    validators: FieldValidator.mustMatch('password', 'confirmPassword')
+  }
+
+  get form() {
+    return this.profileForm.controls;
+  }
+
+  profileForm = this.formBuilder.group({
+    name: ['', [
+      Validators.required
+    ]],
+    lastName: ['', [
+      Validators.required
+    ]],
+    email: ['', [
+      Validators.required,
+      Validators.email
+    ]],
+    phone: ['', [
+      Validators.required,
+      Validators.maxLength(15)
+    ]],
+    personalResume: ['', [
+      Validators.required
+    ]],
+    password: ['', [
+      Validators.required,
+      Validators.minLength(8)
+    ]],
+    confirmPassword: ['', [
+      Validators.required,
+      Validators.minLength(8)
+    ]]
+  }, this.formOptions)
 }
