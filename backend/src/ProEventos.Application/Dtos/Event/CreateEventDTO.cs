a9c0910b27f289729732cbbd59a8bc94f492ee37
gomesbrnn <brngomesds@gmail.com>
2023-04-19 14:23:48 -0300

    feat: Creating new event dtos for each situations

---
+++ backend/src/ProEventos.Application/Dtos/Event/CreateEventDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Dtos.Event
+{
+    public class CreateEventDTO
+    {
+        public string City { get; set; }
+        public string Date { get; set; }
+        public string Theme { get; set; }
+        public int AmountPeople { get; set; }
+        public string ImageURL { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
