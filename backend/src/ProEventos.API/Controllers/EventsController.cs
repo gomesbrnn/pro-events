53926a1680e50a68724e6b99a6ee258ea6936d4c
gomesbrnn <brngomesds@gmail.com>
2023-04-19 11:21:29 -0300

    refactor: removing unecessary usings

--- backend/src/ProEventos.API/Controllers/EventsController.cs
+++ backend/src/ProEventos.API/Controllers/EventsController.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using ProEventos.Application.Interfaces;
 using ProEventos.Domain.Models;
-using ProEventos.Persistence.Context;
 
 namespace ProEventos.API.Controllers
 {
