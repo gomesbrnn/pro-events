121a74ff0419bdb470e5aac041be688a04a133b3
gomesbrnn <brngomesds@gmail.com>
2023-04-19 14:58:39 -0300

    refactor: changing event service interface params

--- backend/src/ProEventos.Application/Interfaces/IEventService.cs
+++ backend/src/ProEventos.Application/Interfaces/IEventService.cs
@@ -1,15 +1,15 @@
 using System.Threading.Tasks;
-using ProEventos.Domain.Models;
+using ProEventos.Application.Dtos.Event;
 
 namespace ProEventos.Application.Interfaces
 {
     public interface IEventService
     {
-        Task<Event[]> GetAllEventsAsync(bool includeSpeakers = false);
-        Task<Event[]> GetAllEventsByThemeAsync(string theme, bool includeSpeakers = false);
-        Task<Event> GetEventByIdAsync(int eventId, bool includeSpeakers = false);
-        Task<Event> AddEvent(Event model);
-        Task<Event> UpdateEvent(int eventId, Event model);
-        Task<Event> DeleteEvent(int eventId);
+        Task<ReadEventDTO[]> GetAllEventsAsync(bool includeSpeakers = false);
+        Task<ReadEventDTO[]> GetAllEventsByThemeAsync(string theme, bool includeSpeakers = false);
+        Task<ReadEventDTO> GetEventByIdAsync(int eventId, bool includeSpeakers = false);
+        Task<ReadEventDTO> AddEvent(CreateEventDTO eventDTO);
+        Task<ReadEventDTO> UpdateEvent(int eventId, UpdateEventDTO eventDTO);
+        Task<ReadEventDTO> DeleteEvent(int eventId);
     }
 }
