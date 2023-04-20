5f937352714af0c0162fdde021dcdbc8f046b2d6
gomesbrnn <brngomesds@gmail.com>
2023-04-20 13:16:31 -0300

    feat: creating speaker dtos

---
+++ backend/src/ProEventos.Application/Dtos/Speaker/ReadSpeakerDTO.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Application.Dtos.Event;
+using ProEventos.Application.Dtos.SocialMedia;
+
+namespace ProEventos.Application.Dtos.Speaker
+{
+    public class ReadSpeakerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PersonalResume { get; set; }
+        public string ImageURL { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<CreateSocialMediaDTO> SocialMedia { get; set; }
+        public IEnumerable<CreateEventDTO> Events { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ backend/src/ProEventos.Application/Dtos/Speaker/UpdateSpeakerDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Dtos.Speaker
+{
+    public class UpdateSpeakerDTO
+    {
+        public string Name { get; set; }
+        public string PersonalResume { get; set; }
+        public string ImageURL { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<int> SocialMediaId { get; set; }
+        public IEnumerable<int> EventsId { get; set; }
+        public bool Status { get; set; }
+    }
+}
