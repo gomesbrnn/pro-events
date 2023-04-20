3540cd61751f1069f34101ab1b3f61d8e97e361a
gomesbrnn <brngomesds@gmail.com>
2023-04-20 16:53:20 -0300

    refactor: removing unnecessary usings

--- backend/src/ProEventos.Application/Dtos/Lot/ReadLotDTO.cs
+++ backend/src/ProEventos.Application/Dtos/Lot/ReadLotDTO.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ProEventos.Application.Dtos.Event;
 
 namespace ProEventos.Application.Dtos.Lot
--- backend/src/ProEventos.Application/Dtos/Speaker/ReadSpeakerDTO.cs
+++ backend/src/ProEventos.Application/Dtos/Speaker/ReadSpeakerDTO.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ProEventos.Application.Dtos.Event;
 using ProEventos.Application.Dtos.SocialMedia;
 
