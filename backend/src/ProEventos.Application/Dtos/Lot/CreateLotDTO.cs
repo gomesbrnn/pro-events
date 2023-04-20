88f8df41ce1cbf0377155cf652f65986e9bda73f
gomesbrnn <brngomesds@gmail.com>
2023-04-20 16:40:42 -0300

    feat: lot dtos data annotations

--- backend/src/ProEventos.Application/Dtos/Lot/CreateLotDTO.cs
+++ backend/src/ProEventos.Application/Dtos/Lot/CreateLotDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,26 @@ namespace ProEventos.Application.Dtos.Lot
 {
     public class CreateLotDTO
     {
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+
+        [Required]
+        [Range(0.01, 10000.00)]
         public decimal Price { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public string StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public string EndDate { get; set; }
+
+        [Required]
+        [Range(1, 150)]
         public int Quantity { get; set; }
         public int EventId { get; set; }
     }
