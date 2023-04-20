d9ef62e1e1dbaead2a692624670ae240227f277f
gomesbrnn <brngomesds@gmail.com>
2023-04-20 15:53:37 -0300

    feat: event dtos data annotations

--- backend/src/ProEventos.Application/Dtos/Event/CreateEventDTO.cs
+++ backend/src/ProEventos.Application/Dtos/Event/CreateEventDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,32 @@ namespace ProEventos.Application.Dtos.Event
 {
     public class CreateEventDTO
     {
+        [Required]
+        [StringLength(30)]
         public string City { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public string Date { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Theme { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int AmountPeople { get; set; }
+
         public string ImageURL { get; set; }
+
+        [Phone]
         public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
         public IEnumerable<int> LotsId { get; set; }
         public IEnumerable<int> SocialMediaId { get; set; }
         public IEnumerable<int> SpeakersId { get; set; }
