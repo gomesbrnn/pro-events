d61d3732576ceac8282b9429b0cb34c457ee889c
gomesbrnn <brngomesds@gmail.com>
2023-04-20 16:50:42 -0300

    feat: speaker dtos data annotations

--- backend/src/ProEventos.Application/Dtos/Speaker/CreateSpeakerDTO.cs
+++ backend/src/ProEventos.Application/Dtos/Speaker/CreateSpeakerDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,25 @@ namespace ProEventos.Application.Dtos.Speaker
 {
     public class CreateSpeakerDTO
     {
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(200)]
         public string PersonalResume { get; set; }
+
+        [RegularExpression(@"/.*\.(gif|jpe?g|bmp|png)$/igm")]
         public string ImageURL { get; set; }
+
+        [Required]
+        [Phone]
         public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
         public IEnumerable<int> SocialMediaId { get; set; }
         public IEnumerable<int> EventsId { get; set; }
     }
