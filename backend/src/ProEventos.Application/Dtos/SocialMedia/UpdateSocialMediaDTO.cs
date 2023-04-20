860d6a6d78b32da37c08af437ea0d0e53b5e5ba0
gomesbrnn <brngomesds@gmail.com>
2023-04-20 16:46:09 -0300

    feat: socialmedia dtos data annotations

--- backend/src/ProEventos.Application/Dtos/SocialMedia/UpdateSocialMediaDTO.cs
+++ backend/src/ProEventos.Application/Dtos/SocialMedia/UpdateSocialMediaDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace ProEventos.Application.Dtos.SocialMedia
 {
     public class UpdateSocialMediaDTO
     {
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
+
+        [Required]
+        [Url]
         public string URL { get; set; }
+
         public int EventId { get; set; }
         public int SpeakerId { get; set; }
         public bool Status { get; set; }
