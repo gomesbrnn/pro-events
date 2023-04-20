c9682d26409c90dd9a6c851b1d88f563fd59c31b
gomesbrnn <brngomesds@gmail.com>
2023-04-20 15:07:49 -0300

    feat: new mappings for dtos

--- backend/src/ProEventos.Application/Mappings/MappingProfile.cs
+++ backend/src/ProEventos.Application/Mappings/MappingProfile.cs
@@ -1,5 +1,8 @@
 using AutoMapper;
 using ProEventos.Application.Dtos.Event;
+using ProEventos.Application.Dtos.Lot;
+using ProEventos.Application.Dtos.SocialMedia;
+using ProEventos.Application.Dtos.Speaker;
 using ProEventos.Domain.Models;
 
 namespace ProEventos.Application.Mappings
@@ -8,9 +11,21 @@ namespace ProEventos.Application.Mappings
     {
         public MappingProfile()
         {
-            CreateMap<Event, ReadEventDTO>().ReverseMap();
             CreateMap<Event, CreateEventDTO>().ReverseMap();
+            CreateMap<Event, ReadEventDTO>().ReverseMap();
             CreateMap<Event, UpdateEventDTO>().ReverseMap();
+
+            CreateMap<Lot, CreateLotDTO>().ReverseMap();
+            CreateMap<Lot, ReadLotDTO>().ReverseMap();
+            CreateMap<Lot, UpdateLotDTO>().ReverseMap();
+
+            CreateMap<SocialMedia, CreateSocialMediaDTO>().ReverseMap();
+            CreateMap<SocialMedia, ReadSocialMediaDTO>().ReverseMap();
+            CreateMap<SocialMedia, UpdateSocialMediaDTO>().ReverseMap();
+
+            CreateMap<Speaker, CreateSpeakerDTO>().ReverseMap();
+            CreateMap<Speaker, ReadSpeakerDTO>().ReverseMap();
+            CreateMap<Speaker, UpdateSpeakerDTO>().ReverseMap();
         }
     }
 }
