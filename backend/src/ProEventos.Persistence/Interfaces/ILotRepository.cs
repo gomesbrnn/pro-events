ee2f88f24c161e98c40033989c53ed67d1e1ada8
gomesbrnn <brngomesds@gmail.com>
2023-05-03 17:05:40 -0300

    feat: creating an interface of lot repository

---
+++ backend/src/ProEventos.Persistence/Interfaces/ILotRepository.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using ProEventos.Domain.Models;
+
+namespace ProEventos.Persistence.Interfaces
+{
+    public interface ILotRepository
+    {
+        /// <summary>
+        /// Get method that returns a collection of lots by eventId
+        /// </summary>
+        /// <param name="eventId">Primary key from event table<</param>
+        /// <returns>Array of lots</returns>
+        Task<Lot[]> GetLotsByEventIdAsync(int eventId);
+
+        /// <summary>
+        /// Get method that returns one lot
+        /// </summary>
+        /// <param name="eventId">Primary key from event table</param>
+        /// <param name="id">Primary key from lot table</param>
+        /// <returns>Just one lot</returns>
+        Task<Lot> GetLotByIds(int eventId, int id);
+
+        /// <summary>
+        /// Delete method that inactivates one lot
+        /// </summary>
+        /// <param name="eventId">Primary key from event table<</param>
+        /// <returns>The lot that been deleted</returns>
+        Task<Lot> DeleteLotByEventId(int eventId);
+    }
+}
