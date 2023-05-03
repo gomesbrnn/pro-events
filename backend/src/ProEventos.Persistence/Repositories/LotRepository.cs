8089e9eec80db090834d37fe50a9f8a6a7b67ce4
gomesbrnn <brngomesds@gmail.com>
2023-05-03 17:27:54 -0300

    feat: implementing interface of lot repository

---
+++ backend/src/ProEventos.Persistence/Repositories/LotRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain.Models;
+using ProEventos.Persistence.Context;
+using ProEventos.Persistence.Interfaces;
+
+namespace ProEventos.Persistence.Repositories
+{
+    public class LotRepository : ILotRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LotRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Lot[]> GetLotsByEventIdAsync(int eventId)
+        {
+            IQueryable<Lot> query = _context.Lots.Where(lot =>
+                                                  lot.EventId == eventId &&
+                                                  lot.Status == true)
+                                                 .AsNoTracking();
+
+            return await query.ToArrayAsync();
+        }
+        public async Task<Lot> GetLotByIds(int eventId, int id)
+        {
+            IQueryable<Lot> query = _context.Lots.Where(lot =>
+                                                  lot.EventId == eventId &&
+                                                  lot.Id == id &&
+                                                  lot.Status == true)
+                                                 .AsNoTracking();
+            return await query.FirstAsync();
+        }
+
+        public async Task<Lot> DeleteLotByEventId(int eventId)
+        {
+            IQueryable<Lot> query = _context.Lots.Where(lot =>
+                                                  lot.EventId == eventId &&
+                                                  lot.Status == true)
+                                                 .AsNoTracking();
+
+            return await query.FirstAsync();
+        }
+    }
+}
