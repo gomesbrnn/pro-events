d28cacac0cb25c3b0eaced42dc67be4125f00067
gomesbrnn <brngomesds@gmail.com>
2023-05-03 14:43:45 -0300

    perf: unsubscribing from service after she's called

--- frontend/ProEventos-App/src/app/services/event.service.ts
+++ frontend/ProEventos-App/src/app/services/event.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Event } from '../models/event';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,26 +14,38 @@ export class EventService {
   constructor(private http: HttpClient) { }
 
   public getEvents(): Observable<Event[]> {
-    return this.http.get<Event[]>(this.baseUrl)
+    return this.http
+      .get<Event[]>(this.baseUrl)
+      .pipe(take(1));
   }
 
   public getEventsByTheme(theme: string): Observable<Event[]> {
-    return this.http.get<Event[]>(this.baseUrl + `theme/${theme}`)
+    return this.http
+      .get<Event[]>(this.baseUrl + `theme/${theme}`)
+      .pipe(take(1));
   }
 
   public getEventById(id: number): Observable<Event> {
-    return this.http.get<Event>(this.baseUrl + `${id}`)
+    return this.http
+      .get<Event>(this.baseUrl + `${id}`)
+      .pipe(take(1));
   }
 
   public createEvent(event: Event): Observable<Event> {
-    return this.http.post<Event>(this.baseUrl, event)
+    return this.http
+      .post<Event>(this.baseUrl, event)
+      .pipe(take(1));
   }
 
   public updateEvent(event: Event, eventId: number): Observable<Event> {
-    return this.http.put<Event>(this.baseUrl + `${eventId}`, event)
+    return this.http
+      .put<Event>(this.baseUrl + `${eventId}`, event)
+      .pipe(take(1));
   }
 
   public deleteEvent(eventId: number): Observable<Event> {
-    return this.http.delete<Event>(this.baseUrl + `${eventId}`)
+    return this.http
+      .delete<Event>(this.baseUrl + `${eventId}`)
+      .pipe(take(1));
   }
 }
